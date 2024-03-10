4132d275ef89ea6fe47060213789d67541f99fed
hieunqdev <hieunqdev@gmail.com>
2024-03-11 01:21:13 +0700

    AUTHENTICATION: Login uses cookie, session

---
+++ Lazada/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lazada.Entity;
+
+namespace Lazada.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly LazadaDbContext _context;
+        public AccountController(LazadaDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            var account = (from user in _context.Account
+                           where user.UserName == email && user.Password == password
+                           select new { 
+                               user.UserName, 
+                               user.Password 
+                           }).FirstOrDefault();
+            if (account != null)
+            {
+                return Redirect(Url.Action("Index", "Home"));
+            }
+            return View();
+            
+        }
+    }
+}
---
+++ Lazada/Entity/LazadaDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Lazada.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lazada.Entity
+{
+    public class LazadaDbContext : DbContext
+    {
+        public LazadaDbContext(DbContextOptions<LazadaDbContext> options) : base(options) { }
+        public DbSet<Account> Account { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.Property(entity => entity.IsActive)
+                      .HasDefaultValue(true);
+                      
+            });
+        }
+    }
+}
